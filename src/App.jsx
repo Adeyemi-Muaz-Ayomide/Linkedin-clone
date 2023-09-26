c116f0f5ac5e925b6c12f1135c9152d8b9701e8f
iAmMuadh <adeyemimuaz1@gmail.com>
2023-09-26 21:24:14 +0100

    rtk used to improve functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useSelector } from "react-redux";
 import Feed from "./components/AppBody/Feed";
 import Sidebar from "./components/AppBody/Sidebar";
 import Header from "./components/Header/Header";
 import "./index.css";
+import { selectUser } from "./features/UserSlice";
+import Login from "./components/Login/Login";
 const App = () => {
+  const user = useSelector(selectUser);
   return (
     <div className="bg-[#f3f2ef] flex-1 items-center flex-col">
       <Header />
 
-      {/* App Body */}
-      <div className="flex">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          {/* App Body */}
+          <div className="flex">
+            <Sidebar />
+            <Feed />
+          </div>
+        </>
+      )}
     </div>
   );
 };
